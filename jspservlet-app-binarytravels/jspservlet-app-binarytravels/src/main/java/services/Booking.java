285e9c16fb30aabf7ea72ce6ea90d45c4ee33f50
Iamneo <techadmin@iamneo.ai>
2023-08-12 05:34:11 +0000

    Backup Commit

--- jspservlet-app-binarytravels/jspservlet-app-binarytravels/src/main/java/services/Booking.java
+++ jspservlet-app-binarytravels/jspservlet-app-binarytravels/src/main/java/services/Booking.java
@@ -1,13 +1,6 @@
-
 package services;
 
 public class Booking{
-
-    public double booking(int noOfPersons,int rates)
-    {
-        
-        return noOfPersons * rates;
-    }
-
+//	Enter code here...
 }
 
